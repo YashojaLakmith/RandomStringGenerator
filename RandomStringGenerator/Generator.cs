51ccd6ba574ec03c084fb57fb4f9f6dc9ea1df9a
Yashoja Lakmith <yashoja002@gmail.com>
2023-05-24 23:14:49 +0530

    Add project files.

---
+++ RandomStringGenerator/Generator.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RandomStringGenerator
+{
+    /// <summary>
+    /// Class for providing API for generating random strings. This class cannot be inherited.
+    /// </summary>
+    public sealed class Generator
+    {
+        /// <summary>
+        /// Generates a random string using a cryptographically strong Random Number Generator.
+        /// </summary>
+        /// <param name="characters">Character set that will be used. The number of characters that will be used (After ignoring duplicates or not) should not be less than 2</param>
+        /// <param name="length">Required character length of the random string. This should not be less than 1.</param>
+        /// <param name="ignoreDuplicates">Whether the duplicates in the character set should be ignored. (Ignored by default)</param>
+        /// <returns>A random string based on given parameters.</returns>
+        /// <exception cref="ArgumentException"></exception>
+        public static string GenerateRandomString(string characters, int length, bool ignoreDuplicates = true)
+        {
+            if (length < 1)
+            {
+                throw new ArgumentException("Length of the output string should not be less than to 1");
+            }
+
+            if(string.IsNullOrEmpty(characters))
+            {
+                throw new ArgumentException("The character string should not be null, empty or only whitespaces");
+            }
+
+            if (string.IsNullOrWhiteSpace(characters))
+            {
+                throw new ArgumentException("The character string should not be null, empty or only whitespaces");
+            }
+
+            int charsetLength = characters.Length;
+
+            if (!ignoreDuplicates)
+            {
+                if (charsetLength < 2)
+                {
+                    throw new ArgumentException("The size of the character set should not be less than 2");
+                }
+
+                using (ServiceProvider provider = new ServiceProvider())
+                {
+                    return provider.GenerateUsingRNG(characters, charsetLength, length);
+                }
+            }
+
+            IList<char> uniques = new List<char>();
+            uniques.Add(characters[0]);
+
+            foreach (char c in characters)
+            {
+                if (!uniques.Contains(c))
+                {
+                    uniques.Add(c);
+                }
+            }
+
+            charsetLength = uniques.Count;
+            if (charsetLength < 2)
+            {
+                throw new ArgumentException("The size of the character set after ignoring duplicates should not be less than 2");
+            }
+
+            using (ServiceProvider provider = new ServiceProvider())
+            {
+                return provider.GenerateUsingRNG(new string(uniques.ToArray()), charsetLength, length);
+            }
+        }
+    }
+}
---
+++ RandomStringGenerator/ServiceProvider.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace RandomStringGenerator
+{
+    /// <summary>
+    /// Class for providing access for Cryptographic services. This class cannot be inherited.
+    /// </summary>
+    internal sealed class ServiceProvider : IDisposable
+    {
+        private char[]? buffer;
+        private bool disposed;
+
+        /// <summary>
+        /// Generates a random string based on given parameters using .NET cryptographic library's Random Number Generator.
+        /// </summary>
+        /// <param name="characters">Character set to be used.</param>
+        /// <param name="charArrayLength">Size of the character set.</param>
+        /// <param name="length">Required length of the random string.</param>
+        /// <returns>A random string based on given parameters.</returns>
+        /// <exception cref="ObjectDisposedException"></exception>
+        internal string GenerateUsingRNG(string characters, int charArrayLength, int length)
+        {
+            if (disposed)
+            {
+                throw new ObjectDisposedException($"The object {GetType().FullName} has been disposed.");
+            }
+
+            buffer = new char[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                buffer[i] = characters[RandomNumberGenerator.GetInt32(charArrayLength)];
+            }
+
+            return new string(buffer);
+        }
+
+        /// <summary>
+        /// Disposes of all resources.
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// Explicitely nullifies used variables.
+        /// </summary>
+        /// <param name="disposing"></param>
+        private void Dispose(bool disposing)
+        {
+            if(!disposed)
+            {
+                buffer = null;
+                disposed = true;
+            }
+        }
+    }
+}
